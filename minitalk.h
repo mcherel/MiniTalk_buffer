44c211b62503819efef14fc8f171090021b23e15
Maria CHEREL SOROKINA <mcherel-@e3r12p9.clusters.42paris.fr>
2022-05-18 15:20:05 +0200

    minor

--- minitalk.h
+++ minitalk.h
@@ -9,13 +9,13 @@
  # define PID_INVALID   "ERROR: Invalid server PID\n"
  # define NBA_INVALID   "ERROR: Invalid number of argumets. Allowed : "
  # define STR_EMPTY     "ERROR: Empty string!!! \n"
- # define SIG_ERROR     "ERROR: Signal problem!!! \n"
+ //# define SIG_ERROR     "ERROR: Signal problem!!! \n"
 
 //MESSAGES
  # define MSG_WELCOME "Welcome, PID: "
  # define MSG_DELIVRD "Your message was delivered !!!!\n"
 
 //CONSTANTES
-# define SLEEP_TIME	200000
+# define SLEEP_TIME	20000
 
 # endif
